be3c4f43036bf4b37fd046e17fb688432643f9fd
Ramana2130 <ramanaraj7143@gmail.com>
2024-04-06 12:23:14 +0530

    loading updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import { useState } from 'react'
-import About from './components/About'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Skills from './components/skills'
-import Work from './components/Work'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import { useState } from "react";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
-    <>
-    <Navbar/>
-    <Hero/>
-    <Skills/>
-    <About/>
-    <Work/>
-    <Contact/>
-    <Footer/>
-    </>
-  )
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
+      <div>
+        {loading ? (
+          <ClipLoader color={"#f46e69"} loading={loading} size={150} />
+        ) : (
+          <>
+            <Navbar />
+            <Hero />
+            <Skills />
+            <About />
+            <Work />
+            <Contact />
+            <Footer />
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
